edc039d2ccc601785fe86d2ec829d31970fe34f7
Joseph Nichollas <nichollascavalcante@gmail.com>
2025-09-02 13:53:30 -0300

    Implementa TarefaRepository - SRP e DIP aplicados

---
+++ projeto_ToDoList/repositorios/TarefaRepository.java
@@ -0,0 +1,53 @@
+package repositorios;
+
+import interfaces.ITarefaRepository;
+import modelo.Tarefa;
+import controle.ManipuladorDeTarefas;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Implementação do repositório de tarefas
+ * Princípio SRP: Responsabilidade única de persistência
+ * Princípio DIP: Implementa abstração ITarefaRepository
+ */
+public class TarefaRepository implements ITarefaRepository {
+    private ManipuladorDeTarefas manipulador;
+    
+    public TarefaRepository(ManipuladorDeTarefas manipulador) {
+        this.manipulador = manipulador;
+    }
+    
+    @Override
+    public void salvar(Tarefa tarefa) {
+        manipulador.adicionarTarefa(tarefa);
+    }
+    
+    @Override
+    public void remover(Tarefa tarefa) {
+        manipulador.removerTarefa(tarefa);
+    }
+    
+    @Override
+    public void atualizar(Tarefa antiga, Tarefa nova) {
+        manipulador.editarTarefa(antiga, nova);
+    }
+    
+    @Override
+    public List<Tarefa> listarTodas() {
+        return manipulador.listarTarefas();
+    }
+    
+    @Override
+    public List<Tarefa> listarPorData(LocalDate data) {
+        return manipulador.listarTarefasPorData(data);
+    }
+    
+    @Override
+    public Tarefa buscarPorTitulo(String titulo) {
+        return listarTodas().stream()
+            .filter(t -> t.getTitulo().equalsIgnoreCase(titulo))
+            .findFirst()
+            .orElse(null);
+    }
+}
